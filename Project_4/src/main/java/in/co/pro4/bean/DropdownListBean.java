5592a54ba55788a0b4110a4ebccf2cc294178bd8
BasantVishwakarma0448 <basantvishwakarma0448@gmail.com>
2023-01-03 15:02:58 +0530

    ORS

---
+++ Project_4/src/main/java/in/co/pro4/bean/DropdownListBean.java
@@ -0,0 +1,12 @@
+package in.co.pro4.bean;
+
+/**
+ * @author Basant Vishwakarma
+ *
+ */
+public interface DropdownListBean {
+	
+	public String getKey();
+	
+	public String getValue();
+}
---
+++ Project_4/src/main/java/in/co/pro4/exception/DuplicateRecordException.java
@@ -0,0 +1,14 @@
+package in.co.pro4.exception;
+
+/**
+ * DuplicateRecordException thrown when a duplicate record occurred.
+ * 
+ * @author Basant Vishwakarma
+ *
+ */
+public class DuplicateRecordException extends Exception {
+
+	public DuplicateRecordException(String msg) {
+		super(msg);
+	}
+}
